ac834051e0d129bc63958a20eeed32aa5fe3e5fc
Samuel Park <s.park111497@gmail.com>
2022-09-29 00:24:47 -0700

    server practice completed

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 const http = require('http');
 const fs = require("fs");
 
+
 const server = http.createServer((req, res) => {
-  // Your code here
+  // parse the request URL path
+  let reqPath = req.url.split('/');
+
+  // if the URL path starts with the keyword 'static'
+  // send the specific file that is being requested as the response
+  if (reqPath[1] === 'static') {
+    let fileType;
+
+    // set variable for the requested file type
+    const reqType = req.url.split('.')[1];
+
+    // set variable for the request file's folder path
+
+    const staticFile = fs.readFileSync('./assets/' + reqPath.slice(2).join('/'));
+
+    // check the file type and assign it to fileType
+    if (reqType === 'css') {
+      fileType ='text/CSS';
+    }
+
+    if (reqType === 'jpg') {
+      fileType ='image/jpeg';
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', fileType);
+    return res.end(staticFile);
+    
+    }
+
+  // load the html file
+  const html = fs.readFileSync('./index.html', 'utf-8');
+
+  // send response
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(html);
 });
 
 const port = 5000;
